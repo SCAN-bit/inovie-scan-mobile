ce87db2e6e3672f5914e2a362819e34e7f224202
mickaelvolletech@gmail.com <mickaelvolletech@gmail.com>
2025-09-18 09:37:12 +0200

    Fix MainApplication.kt compilation errors - remove deprecated APIs and use Android built-in JS engine

--- android/app/src/main/java/SCAN/mobile/MainApplication.kt
+++ android/app/src/main/java/SCAN/mobile/MainApplication.kt
@@ -7,68 +7,32 @@ import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.ReactHost
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.defaults.DefaultJSExecutorFactory
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
-import com.facebook.react.ReactInstanceManagerBuilder
-import com.facebook.react.ReactInstanceManager
 
 import expo.modules.ApplicationLifecycleDispatcher
-import expo.modules.ReactNativeHostWrapper
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
-        this,
-        object : DefaultReactNativeHost(this) {
-          override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
-            return packages
-          }
+  override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
+    override fun getPackages(): List<ReactPackage> {
+      val packages = PackageList(this).packages
+      // Packages that cannot be autolinked yet can be added manually here, for example:
+      // packages.add(new MyReactNativePackage());
+      return packages
+    }
 
-          override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
+    override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
 
-          override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+    override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-          override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-          override val isHermesEnabled: Boolean = false
-          
-          // getJSExecutorFactory() removed - let React Native use default Android JS engine
-          
-          override fun createReactInstanceManager(): ReactInstanceManager {
-            val builder = ReactInstanceManagerBuilder.newBuilder()
-                .setApplication(application)
-                .setJSMainModuleName(getJSMainModuleName())
-                .setUseDeveloperSupport(getUseDeveloperSupport())
-                .setRedBoxHandler(getRedBoxHandler())
-                .setJavaScriptExecutorFactory(null) // Force Android's built-in JS engine
-                .setUIImplementationProvider(getUIImplementationProvider())
-                .setJSIModulesPackage(getJSIModulesPackage())
-                .setInitialLifecycleState(LifecycleState.BEFORE_CREATE)
-            
-            for (reactPackage in getPackages()) {
-                builder.addPackage(reactPackage)
-            }
-            
-            val jsBundleFile = getJSBundleFile()
-            if (jsBundleFile != null) {
-                builder.setJSBundleFile(jsBundleFile)
-            } else {
-                builder.setBundleAssetName(getBundleAssetName())
-            }
-            
-            return builder.build()
-          }
-      }
-  )
-
-  override val reactHost: ReactHost
-    get() = ReactNativeHostWrapper.createReactHost(applicationContext, reactNativeHost)
+    override val isNewArchEnabled: Boolean = false
+    override val isHermesEnabled: Boolean = false
+    
+    // Using Android's built-in JavaScript engine - no custom JS executor needed
+  }
 
   override fun onCreate() {
     super.onCreate()
