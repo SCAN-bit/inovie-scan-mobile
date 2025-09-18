1b62036f619a4c35834fc1ae5ee28a2e606d8e52
mickaelvolletech@gmail.com <mickaelvolletech@gmail.com>
2025-09-18 10:10:46 +0200

    Fix Kotlin compilation errors - remove non-existent DefaultJSExecutorFactory and getJSExecutorFactory method

--- android/app/src/main/java/SCAN/mobile/MainApplication.kt
+++ android/app/src/main/java/SCAN/mobile/MainApplication.kt
@@ -9,7 +9,6 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.defaults.DefaultJSExecutorFactory
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
 
@@ -32,10 +31,7 @@ class MainApplication : Application(), ReactApplication {
     override val isNewArchEnabled: Boolean = false
     override val isHermesEnabled: Boolean = false
     
-    // Force Android's built-in JavaScript engine
-    override fun getJSExecutorFactory(): com.facebook.react.jscexecutor.JSCExecutorFactory? {
-      return null // Use Android's built-in JS engine instead of JSC
-    }
+    // Using Android's built-in JavaScript engine - no custom JS executor needed
   }
 
   override fun onCreate() {
