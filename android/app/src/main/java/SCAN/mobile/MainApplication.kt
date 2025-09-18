0a6172f0ab77eb8a11def9137d3ca539ed2ec7f8
mickaelvolletech@gmail.com <mickaelvolletech@gmail.com>
2025-09-18 10:51:11 +0200

    Fix MainApplication.kt imports and use Expo modules for SDK 51 compatibility

--- android/app/src/main/java/SCAN/mobile/MainApplication.kt
+++ android/app/src/main/java/SCAN/mobile/MainApplication.kt
@@ -3,49 +3,52 @@ package SCAN.mobile
 import android.app.Application
 import android.content.res.Configuration
 
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import expo.modules.ApplicationLifecycleDispatcher
+import expo.modules.ReactNativeHostWrapper
 
 // Expo modules removed - using standard React Native
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-    override fun getPackages(): List<ReactPackage> {
-      val packages = PackageList(this).packages
-      // Packages that cannot be autolinked yet can be added manually here, for example:
-      // packages.add(new MyReactNativePackage());
-      return packages
-    }
+  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(
+    this,
+    object : DefaultReactNativeHost(this) {
+      override fun getPackages(): List<ReactPackage> {
+        val packages = expo.modules.PackageList(this).packages
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // packages.add(new MyReactNativePackage());
+        return packages
+      }
 
     override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
 
     override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
 
-    override val isNewArchEnabled: Boolean = false
-    override val isHermesEnabled: Boolean = false
-    
-    // Using Android's built-in JavaScript engine - no custom JS executor needed
-  }
+      override val isNewArchEnabled: Boolean = false
+      override val isHermesEnabled: Boolean = false
+      
+      // Using Android's built-in JavaScript engine - no custom JS executor needed
+    }
+  )
 
   override fun onCreate() {
     super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
+    SoLoader.init(this, false)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
-    // Expo lifecycle dispatcher removed - using standard React Native
+    ApplicationLifecycleDispatcher.onApplicationCreate(this)
   }
 
   override fun onConfigurationChanged(newConfig: Configuration) {
     super.onConfigurationChanged(newConfig)
-    // Expo lifecycle dispatcher removed - using standard React Native
+    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig)
   }
 }
