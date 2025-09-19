05f0cb14a2bc91ea70e976d318ea1f6544f421ea
mickaelvolletech@gmail.com <mickaelvolletech@gmail.com>
2025-09-19 17:06:56 +0200

    Fix: Initialisation forcée expo-modules-core pour éviter globalThis.expo.NativeModule undefined

--- android/version.js
+++ android/version.js
@@ -2,8 +2,8 @@
 // Ce fichier est mis à jour automatiquement par le script de build 
  
 const version = ""; 
-const buildNumber = 20; 
-const fullVersion = ".20"; 
+const buildNumber = 62; 
+const fullVersion = ".62"; 
  
 module.exports = { 
   version, 
