bbe7f9dc00dc081a4719e79ac62f312606e7a88f
mickaelvolletech@gmail.com <mickaelvolletech@gmail.com>
2025-09-19 13:12:09 +0200

    Polyfill expo-asset complet - Création d'expo-asset-polyfill.js - Configuration Metro alias pour rediriger expo-font - Module node_modules/expo-asset créé - Solution: module natif + polyfill + alias Metro

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,17 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-// Configuration transformer (sans forcer le profil par défaut qui casse le web)
-// config.transformer reste par défaut pour éviter les conflits web
-
-// Configuration pour inclure le polyfill Expo en premier
+// Configuration resolver avec alias pour expo-asset
 config.resolver = {
   ...config.resolver,
   platforms: ['native', 'android', 'ios', 'web'],
+  alias: {
+    'expo-asset': path.resolve(__dirname, 'expo-asset-polyfill.js')
+  }
 };
-
-// Serializer simplifié pour éviter les conflits web
-// Le polyfill est déjà chargé via index.js, pas besoin de forcer l'inclusion
 
 module.exports = config;
