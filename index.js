80ce68632260c05f62cae5cfc0fbcd2475347d32
mickaelvolletech@gmail.com <mickaelvolletech@gmail.com>
2025-09-19 16:46:12 +0200

    Fix: Correction configuration entrée application - Fix main not registered error

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+// Initialisation d'expo-modules-core en premier
+import 'expo/build/Expo.fx';
 import { registerRootComponent } from 'expo';
 import App from './App';
 
+// Log pour debugging
+console.log('=== SCAN App Starting ===');
+
+// Enregistrement du composant principal
 registerRootComponent(App);
